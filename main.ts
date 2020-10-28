858ca6d2d172fd1fde0dd9366a24772991d797e6
stewart dunn <stewart@kre8.com>
2020-10-28 23:58:01 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+}
